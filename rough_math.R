91ef6db90e24636411f7ed1c336de33dcbeb5e32
Philipp v. Bieberstein <pbieberstein@email.arizona.edu>
2016-04-15 12:28:22 -0700

    completed math behind app

--- rough_math.R
+++ rough_math.R
@@ -1,11 +1,6 @@
-
-swim_pace = 117 # sec /100m     1.57 min/100m
-bike_pace = 20.31 # mi / hr
-run_pace = 7.31 # min / mi
-
-
-
-######### Start with Lubridate
+in_sec <- function(period_time) {
+  period_to_seconds(period_time)
+}
 
 # Swim time
 #swim_pace = seconds_to_period(117) # sec /100m     1.57 min/100m
@@ -19,10 +14,23 @@ T1 <- period(minute=1, second=24)
 
 # Bike time
 bike_pace <- 20.31 # mi / hr
+bike_time <- 1/(bike_pace/24.9/60/60)
+bike_time <- seconds_to_period(bike_time)
+bike_time
 
 # Transition 2
 T2 <- period(minute=1, second=5)
 
-run_pace <- period(minute=7, second=31) # min / mile
+run_pace <- period(minute=7, second=30) # min / mile
+run_time <-period_to_seconds(run_pace)*6.2
+run_time <- seconds_to_period(run_time)
+run_time
 
 
+# Calculate total time in seconds
+total_time <- in_sec(swim_time)+in_sec(T1)+
+  in_sec(bike_time)+ in_sec(T2)+
+  in_sec(run_time)
+  
+# Transoform into readable format
+seconds_to_period(total_time)
