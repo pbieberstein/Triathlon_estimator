dd407ab5667bf5f9698f69a8b994b8d7fc0c6eb8
Philipp v. Bieberstein <pbieberstein@email.arizona.edu>
2016-04-17 10:40:40 -0700

    interface works

--- ui.R
+++ ui.R
@@ -1,26 +1,53 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(pageWithSidebar(
   
   # Application title
-  titlePanel("Triathlon Time Estimator"),
+  headerPanel("Triathlon Time Estimator"),
 
   # Sidebar with a slider input for the number of bins
 
-    fluidRow(
-      h3("Swim Pace"),
-      column(8,
-             column(2, 
-                    numericInput("num",
-                          "Minutes:",
+    sidebarPanel(
+      h4("Swim Pace (min/100m)"),
+      column(12,
+             column(6, 
+                    numericInput("Minutes:",
                           min = 0,
                           max = 60,
-                          value = 1)),
-             column(2, numericInput("num",
-                                    "Seconds:",
+                          value = 1, 
+                          inputId = "swim_min")),
+             column(6, numericInput("Seconds:",
                                     min = 0,
                                     max = 60,
-                                    value = 30))))))
+                                    value = 30,
+                                    inputId = "swim_sec"))),
+      h4("Bike Pace (mph)"),
+      column(12,
+             column(12, 
+                    numericInput(
+                                 label="Mph:",
+                                 min = 0,
+                                 max = 80,
+                                 step=0.1,
+                                 value = 20,
+                                 inputId = "bike_pace"))),
+      h4("Run Pace (min/mile)"),
+      column(12,
+             column(6, 
+                    numericInput("Minutes:",
+                                 min = 0,
+                                 max = 80,
+                                 value = 6,
+                                 inputId = "run_min")),
+             column(6, numericInput("Seconds:",
+                                    min = 0,
+                                    max = 60,
+                                    value = 30,
+                                    inputId = "run_sec"))),
+      br(), br(), br()
+      ),
+  mainPanel(h1("TIME: 2h 30s"))
+  ))
   
   
