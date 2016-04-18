23f444b494ce6460a305eb9501121080bd48baf0
Philipp v. Bieberstein <pbieberstein@email.arizona.edu>
2016-04-17 21:12:11 -0700

    working app

--- server.R
+++ server.R
@@ -24,8 +24,34 @@ shinyServer(function(input, output) {
     hours <- total_time$hour
     minutes <- total_time$minute
     seconds <- round(total_time$.Data)
-    
     cat(hours, ":", minutes, ":", seconds)
   })
+  
+  output$swim_time <- renderPrint({
+    swim_time <- in_sec(period(minute=input$swim_min,second=input$swim_sec))*(input$swim_dist)/100
+    swim_time <- seconds_to_period(swim_time)
+    hours <- swim_time$hour
+    minutes <- swim_time$minute
+    seconds <- round(swim_time$.Data)
+    cat(hours, ":", minutes, ":", seconds)
+  })
+  output$bike_time <- renderPrint({
+    bike_time <- 1/(input$bike_pace/input$bike_dist/60/60)
+    bike_time <- seconds_to_period(bike_time)
+    hours <- bike_time$hour
+    minutes <- bike_time$minute
+    seconds <- round(bike_time$.Data)
+    cat(hours, ":", minutes, ":", seconds)
+  })
+  
+  output$run_time <- renderPrint({
+    run_time <- in_sec(period(minute=input$run_min,second=input$run_sec))*input$run_dist
+    run_time <- seconds_to_period(run_time)
+    hours <- run_time$hour
+    minutes <- run_time$minute
+    seconds <- round(run_time$.Data)
+    cat(hours, ":", minutes, ":", seconds)
+  })
+  
 })
   
--- ui.R
+++ ui.R
@@ -22,6 +22,7 @@ shinyUI(pageWithSidebar(
                                     max = 60,
                                     value = 30,
                                     inputId = "swim_sec"))),
+      helpText(textOutput("swim_time"), align = "center"),
 
       h4("Bike Pace (mph)", align = "center"),
       column(12,
@@ -33,6 +34,7 @@ shinyUI(pageWithSidebar(
                                  step=0.1,
                                  value = 20,
                                  inputId = "bike_pace"))),
+      helpText(textOutput("bike_time"), align = "center"),
       
       h4("Run Pace (min/mile)", align = "center"),
       column(12,
@@ -47,67 +49,89 @@ shinyUI(pageWithSidebar(
                                     max = 60,
                                     value = 30,
                                     inputId = "run_sec"))),
-      br(tags$hr()),
-      h3("Transition Times:", align = "center"),
+      helpText(textOutput("run_time"), align = "center"),
       
-      h4("#1", align = "center"),
-      column(12,
-             column(6, 
-                    numericInput("Minutes:",
-                                 min = 0,
-                                 max = 60,
-                                 value = 1, 
-                                 inputId = "T1_min")),
-             column(6, numericInput("Seconds:",
-                                    min = 0,
-                                    max = 60,
-                                    value = 0,
-                                    inputId = "T1_sec"))),
+      tags$hr(),
       
-      h4("#2", align = "center"),
-      column(12,
-             column(6, 
-                    numericInput("Minutes:",
-                                 min = 0,
-                                 max = 60,
-                                 value = 1, 
-                                 inputId = "T2_min")),
-             column(6, numericInput("Seconds:",
-                                    min = 0,
-                                    max = 60,
-                                    value = 0,
-                                    inputId = "T2_sec"))),
-      # create extra gray space
-      br(), br(), br()
+      # Option of including transition times
+      checkboxInput('showPanel1', 'Include Transition times', FALSE),
+      conditionalPanel(condition = 'input.showPanel1',
+                       wellPanel(
+                         h3("Transition Times:", align = "center"),
+                         
+                         h4("#1", align = "center"),
+                         column(12,
+                                column(6, 
+                                       numericInput("Minutes:",
+                                                    min = 0,
+                                                    max = 60,
+                                                    value = 0, 
+                                                    inputId = "T1_min")),
+                                column(6, numericInput("Seconds:",
+                                                       min = 0,
+                                                       max = 60,
+                                                       value = 0,
+                                                       inputId = "T1_sec"))),
+                         
+                         h4("#2", align = "center"),
+                         column(12,
+                                column(6, 
+                                       numericInput("Minutes:",
+                                                    min = 0,
+                                                    max = 60,
+                                                    value = 0, 
+                                                    inputId = "T2_min")),
+                                column(6, numericInput("Seconds:",
+                                                       min = 0,
+                                                       max = 60,
+                                                       value = 0,
+                                                       inputId = "T2_sec"))),
+                         # create extra gray space
+                         br(), br(), br()
+                       )
+                         
+                         
+                         
+                       )
       ),
+      
+      
+      
   
   # Display total time and adjusting distances
   mainPanel(br(),h1(textOutput("total_time"), align = "center", style = "font-size: 100px; font-size: 14vw;"),
             helpText("(Hr : Min : Sec)", align = "center", style = "font-size: 100px; font-size: 3vw;"),
             br(),br(),br(),br(),br(),
             
-            h3("Adjust Course Distances", align = "center"),
-            helpText("If you know your exact course distances, enter them here:"),
-            column(12,
-                   column(4, 
-                          numericInput("Swim Distance: (m)",
-                                       min = 0,
-                                       max = 10000,
-                                       value = 1500,
-                                       inputId = "swim_dist")),
-                   column(4, 
-                          numericInput("Bike Distance (mi)",
-                                       min = 0,
-                                       max = 200,
-                                       value = 24.9,
-                                       step=0.1,
-                                       inputId = "bike_dist")),
-                   column(4, numericInput("Run Distance (mi):",
-                                          min = 0,
-                                          max = 30,
-                                          value = 6.2,
-                                          step=0.1,
-                                          inputId = "run_dist")))
+            checkboxInput('showPanel2', 'Adjust Distances', FALSE),
+            conditionalPanel(condition = 'input.showPanel2',
+                             wellPanel(
+                               h3("Adjust Course Distances", align = "center"),
+                                       helpText("If you know your exact course distances, enter them here:"),
+                                       column(12,
+                                              column(4, 
+                                                     numericInput("Swim Distance: (m)",
+                                                                  min = 0,
+                                                                  max = 10000,
+                                                                  value = 1500,
+                                                                  inputId = "swim_dist")),
+                                              column(4, 
+                                                     numericInput("Bike Distance (mi)",
+                                                                  min = 0,
+                                                                  max = 200,
+                                                                  value = 24.9,
+                                                                  step=0.1,
+                                                                  inputId = "bike_dist")),
+                                              column(4, numericInput("Run Distance (mi):",
+                                                                     min = 0,
+                                                                     max = 30,
+                                                                     value = 6.2,
+                                                                     step=0.1,
+                                                                     inputId = "run_dist"))),
+                               br(), br(), br(), br()) # So that gray box extends below text
+            )
+            
+            
             
             
             )
